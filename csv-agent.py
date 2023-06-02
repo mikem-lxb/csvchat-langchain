c1336b9f9236b1425eb3fe0778774209483bf9b4
Mike McCune <128094186+mikem-lxb@users.noreply.github.com>
2023-06-02 04:24:50 +0000

    .

--- csv-agent.py
+++ csv-agent.py
@@ -2,7 +2,7 @@ from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 import os
 
-os.environ[ "OPENAI_API_KEY"] = "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"
+os.environ[ "OPENAI_API_KEY"] = "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"
 
 agent = create_csv_agent(OpenAI(temperature=0), 'export.csv', verbose=True)
 
